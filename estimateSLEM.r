27d4a7d0307f8f6c43439ba8e8f37fe872e896d9
student <student@students-MacBook-Pro.local>
2016-01-30 23:07:58 -0500

    Algorithm step2

--- estimateSLEM.r
+++ estimateSLEM.r
@@ -2,12 +2,73 @@
 ## The algorithm to estimate the SLEM ##
 ########################################
 
-# f is any feasible function
-# M is a markov basis of A
-# b is the right-hand side
-# A is a matrix
-# n is an interger
- 
-estimateSLEM=function(f,A,b,M,n){
-  
+#Here I picked 2 by 3 table 
+#with column sums c=(4,4,4) and row sums r=(6,6).
+
+A=matrix(c(1,0,0,1,0,0,
+           0,1,0,0,1,0,
+           0,0,1,0,0,1,
+           1,1,1,0,0,0,
+           0,0,0,1,1,1),nrow=5,ncol=6,byrow=T)
+
+b=c(4,4,4,6,6)
+
+M=matrix(c(1 , 0, 1,
+           0 , 1,-1,
+           -1,-1, 0,
+           -1 , 0,-1,
+           0 ,-1, 1,
+           1, 1, 0),nrow=6,ncol=3,byrow=T)
+
+#This time assume that f is the product of all values in the table.
+f=function(f){
+  return(prod(f))
 }
+
+#Let TT denote how long the "long time"=T
+TT=1000
+
+estimateSLEM=function(A,b,M,f,TT){
+  
+  #pg15 step2
+  
+  #Let the inital state column sums of A
+  
+  X0=rep(NA,ncol(A))
+  
+  for (i in 1:ncol(A)){
+    X0[i]=sum(A[,i])
+  }
+  
+  #Collect samples f(X0),....,f(XTT)
+  
+  samples=rep(NA,TT)
+  
+  samples[1]=f(X0)
+  X=X0
+  
+  for (i in 1:(TT-1)){
+    
+    while(T){
+      
+      s=sample(1:ncol(M),1)
+      t=sample(c(1,-1),1)
+      move=t*M[,s]
+      
+      X1=X+move
+      
+      if (all(X1>=0)){
+      
+        X=X1
+        break
+      } #close if statement
+    }#close while loop
+    
+    samples[(i+1)]=f(X)
+}#close for loop
+  
+  return(samples)
+  
+}#Close the function estimateSLEM
+
+estimateSLEM(A,b,M,f,TT)
