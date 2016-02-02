a7a49e7b4574e8b7b1680fdbd02762f34cc5ea3a
student <student@students-MacBook-Pro.local>
2016-02-01 22:26:35 -0500

    Step3

--- estimateSLEM.r
+++ estimateSLEM.r
@@ -29,18 +29,21 @@ f=function(f){
 #Let TT denote how long the "long time"=T
 TT=1000
 
-estimateSLEM=function(A,u,M,f,TT){
+#Let n denote the "small number" in step 3. Let's just say n=10 for now.
+n=10
+
+estimateSLEM=function(A,u,M,f,TT,n){
   
   #pg15 step2
   
   #Collect samples f(X0),....,f(XTT)
   
-  samples=rep(NA,TT)
+  samples=rep(NA,(TT+1))
   
   samples[1]=f(u)
   X=u
   
-  for (i in 1:(TT-1)){
+  for (i in 1:TT){
     
     while(T){
       
@@ -59,9 +62,35 @@ estimateSLEM=function(A,u,M,f,TT){
     
     samples[(i+1)]=f(X)
 }#close for loop
+
+s=NULL
+t=NULL  
   
-  return(samples)
+#Step3 Estimate the autocovariance function
+#for at lags s=0,1,...,2n-1.
+
+#Autocovariance function for f    
+Cf=rep(NA,2*n)
+#f.hat(XTT)
+f.hat=mean(samples)
+
+for (s in 1:(2*n)){
+  
+  #Let f.3.0.4 denote (f_t-f.hat_T)(f_(t+1)-f.hat_T) in (3.0.4) page 10  
+  f.3.0.4=rep(NA,(TT-abs(s-1)))
+  
+  for (t in 1:(TT-abs(s-1))){
+    f.3.0.4[t]=(samples[t]-f.hat)*(samples[t+1]-f.hat)
+  } # End of for (t in (1:TT-abs(s-1)))
+  
+  Cf[s]=sum(f.3.0.4)/(TT-abs(s-1))
+  
+}# end of for (s in (2*n-1))
+
+return(Cf)
   
 }#Close the function estimateSLEM
 
-estimateSLEM(A,u,M,f,TT)
+#For now, this function returns the autocovariance function for f
+#The length of this out put vector is 2*n
+estimateSLEM(A,u,M,f,TT,n)
