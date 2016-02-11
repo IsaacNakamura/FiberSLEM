55479dad38043c93e888498b4cf8c3b21537326f
IsaacNakamura <ana236@g.uky.edu>
2016-02-10 22:24:55 -0500

    table1 implementation

--- estimateSLEM.r
+++ estimateSLEM.r
@@ -8,21 +8,37 @@ library("geigen")
 #Here I picked 2 by 3 table 
 #with column sums c=(4,4,4) and row sums r=(6,6).
 
-A=matrix(c(1,0,0,1,0,0,
-           0,1,0,0,1,0,
-           0,0,1,0,0,1,
-           1,1,1,0,0,0,
-           0,0,0,1,1,1),nrow=5,ncol=6,byrow=T)
+#A=matrix(c(1,0,0,1,0,0,
+#           0,1,0,0,1,0,
+#           0,0,1,0,0,1,
+#           1,1,1,0,0,0,
+#           0,0,0,1,1,1),nrow=5,ncol=6,byrow=T)
 
 #initial node
-u=matrix(c(2,2,2,2,2,2),nrow=1,ncol=6)
+#u=matrix(c(2,2,2,2,2,2),nrow=1,ncol=6)
 
-M=matrix(c(1 , 0, 1,
-           0 , 1,-1,
-           -1,-1, 0,
-           -1 , 0,-1,
-           0 ,-1, 1,
-           1, 1, 0),nrow=6,ncol=3,byrow=T)
+#M=matrix(c(1 , 0, 1,
+#           0 , 1,-1,
+#           -1,-1, 0,
+#           -1 , 0,-1,
+#           0 ,-1, 1,
+#           1, 1, 0),nrow=6,ncol=3,byrow=T)
+
+library(algstat)
+
+A=matrix(c(1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
+           0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,
+           0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,
+           0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,
+           1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,
+           0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,
+           0,0,1,0,0,0,1,0,0,0,1,0,0,0,1,0,
+           0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,1),nrow=8,ncol=16,byrow=T)
+
+u=matrix(c(2,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0),nrow=1,ncol=16)
+
+M=markov(A)
+
 
 #This time assume that f is the product of all values in the table.
 f=function(f){
